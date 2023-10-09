433db17900be5e4356f13e37de3e90390f95e8f9
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2023-10-09 23:39:45 +0700

    fix bug cart qty

--- src/models/cart.js
+++ src/models/cart.js
@@ -5,12 +5,19 @@ function Cart() {
   this.tinhTotalPrice = function () {
     this.totalPrice = 0;
     this.listProduct.map((item) => {
+      if (item.quantity <= 0) {
+        item.quantity = 0;
+      }
       this.totalPrice += item.product.price * item.quantity;
     });
   };
   this.tinhTotalQuantity = function () {
     this.totalQuantity = 0;
+
     this.listProduct.map((item) => {
+      if (item.quantity <= 0) {
+        item.quantity = 0;
+      }
       this.totalQuantity += item.quantity;
     });
   };
